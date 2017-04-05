c2410720f6f88c2a8e9f745e478a8e407ed6f07d
ondrejd <ondrejd@gmail.com>
2017-04-05 10:10:21 +0200

    Another changes in settings.

--- odwp-edd_addons.php
+++ odwp-edd_addons.php
@@ -153,46 +153,108 @@ endif;
 
 
 
+if ( ! function_exists( 'odwpedda_sanitize_settings' ) ) :
+    /**
+     * @todo Finish this!
+     */
+    function odwpedda_sanitize_settings() {
+        var_dump( $_POST );exit();
+    }
+endif;
+
+
 if ( !function_exists( 'odwpedda_admin_init' ) ) :
     /**
      * Initializes all what is needed for this plugin in WP administration.
      */
     function odwpedda_admin_init() {
-        register_setting( ODWPEDDA_SLUG, ODWPEDDA_OPTSKEY );
+        register_setting( ODWPEDDA_SLUG, ODWPEDDA_OPTSKEY, 'odwpedda_sanitize_settings' );
 
         add_settings_section(
-            'odwpedda_settings_section',
-            __( 'Nastavení pluginu', ODWPEDDA_SLUG ),
-            'odwpedda_settings_section_callback',
-            ODWPEDDA_SLUG
+                'odwpedda_settings_section_0',
+                __( 'Stránka pokladna', ODWPEDDA_SLUG ),
+                'odwpedda_settings_section_0_callback',
+                ODWPEDDA_SLUG
+        );
+
+        add_settings_section(
+                'odwpedda_settings_section_1',
+                __( 'Číselník dokladů', ODWPEDDA_SLUG ),
+                'odwpedda_settings_section_1_callback',
+                ODWPEDDA_SLUG
+        );
+
+        add_settings_section(
+                'odwpedda_settings_section_2',
+                __( 'Generování PDF', ODWPEDDA_SLUG ),
+                'odwpedda_settings_section_2_callback',
+                ODWPEDDA_SLUG
         );
 
         add_settings_field(
-            'odwpedda_settings_field_0',
-            __( 'Omezení výběru zemí', ODWPEDDA_SLUG ),
-            'odwpedda_settings_field_0_render',
-            ODWPEDDA_SLUG,
-            'odwpedda_settings_section'
+                'odwpedda_settings_field_0',
+                __( 'Omezení výběru zemí', ODWPEDDA_SLUG ),
+                'odwpedda_settings_field_0_render',
+                ODWPEDDA_SLUG,
+                'odwpedda_settings_section_0'
         );
 
         add_settings_field(
-            'odwpedda_settings_field_1',
-            __( 'Skrytí checkout polí', ODWPEDDA_SLUG ),
-            'odwpedda_settings_field_1_render',
-            ODWPEDDA_SLUG,
-            'odwpedda_settings_section'
+                'odwpedda_settings_field_1',
+                __( 'Skrytí checkout polí', ODWPEDDA_SLUG ),
+                'odwpedda_settings_field_1_render',
+                ODWPEDDA_SLUG,
+                'odwpedda_settings_section_0'
+        );
+
+        add_settings_field(
+                'odwpedda_settings_field_2',
+                __( 'Začít číselník od', ODWPEDDA_SLUG ),
+                'odwpedda_settings_field_2_render',
+                ODWPEDDA_SLUG,
+                'odwpedda_settings_section_1'
+        );
+
+        add_settings_field(
+                'odwpedda_settings_field_3',
+                __( 'Adresář pro generování', ODWPEDDA_SLUG ),
+                'odwpedda_settings_field_3_render',
+                ODWPEDDA_SLUG,
+                'odwpedda_settings_section_2'
         );
     }
 endif;
 
 
 
-if ( ! function_exists( 'odwpedda_settings_section_callback' ) ) :
+if ( ! function_exists( 'odwpedda_settings_section_0_callback' ) ) :
     /**
-     * Prints settings section.
+     * Prints the first settings section ("Nastavení stránky pokladna").
      */
-    function odwpedda_settings_section_callback() {
-        echo __( 'Níže můžete upravit chování pluginu - v prvním případě můžete vybrat země, na které bude omezen výběr na <em>Checkout</em> stránce; v druhém vybrat pole, které budou na téže stránce pro uživatele schována (některá z těchto polí vyžadují hodnoty a proto jim v případě skrytí budou podsunuty předdefinované hodnoty).', ODWPEDDA_SLUG );
+    function odwpedda_settings_section_0_callback() {
+        echo __( 'Níže můžete upravit stránku pokladna (<em>checkout</em>) - v prvním případě můžete vybrat země, na které bude omezen výběr; v druhém vybrat pole, které budou pro uživatele schována. (Pozn.: Některá z těchto polí vyžadují hodnoty a proto jim v případě skrytí budou podsunuty předdefinované hodnoty - v případě textových polí je to <em>přeskočeno</em>, u PSČ je to <em>12345</em>.).', ODWPEDDA_SLUG );
+    }
+endif;
+
+
+
+if ( ! function_exists( 'odwpedda_settings_section_1_callback' ) ) :
+    /**
+     * Prints the first settings section ("Číselník dokladů").
+     */
+    function odwpedda_settings_section_1_callback() {
+        echo __( 'Nastavení automatického číselníku dokladů.', ODWPEDDA_SLUG );
+    }
+endif;
+
+
+
+if ( ! function_exists( 'odwpedda_settings_section_2_callback' ) ) :
+    /**
+     * Prints the third settings section ("Generování PDF").
+     */
+    function odwpedda_settings_section_2_callback() {
+        echo __( 'Nastavení pro generování dokladů do PDF souborů.', ODWPEDDA_SLUG );
     }
 endif;
 
@@ -253,6 +315,36 @@ endif;
 
 
 
+if ( ! function_exists( 'odwpedda_settings_field_2_render' ) ) :
+    /**
+     * Prints the third settings field.
+     */
+    function odwpedda_settings_field_2_render() {
+        $options = (array) get_option( ODWPEDDA_OPTSKEY );
+        $value   = array_key_exists( 'odwpedda_select_field_2', $options ) ? $options['odwpedda_select_field_2'] : '1';
+?>
+    <input class="small-text" id="odwpedda_select_field_2" id="odwpedda_settings[odwpedda_select_field_2]" type="number">
+<?php
+    }
+endif;
+
+
+
+if ( ! function_exists( 'odwpedda_settings_field_3_render' ) ) :
+    /**
+     * Prints the third settings field.
+     */
+    function odwpedda_settings_field_3_render() {
+        $options = (array) get_option( ODWPEDDA_OPTSKEY );
+        $value   = array_key_exists( 'odwpedda_select_field_3', $options ) ? $options['odwpedda_select_field_3'] : 'edd_pdf';
+?>
+    <input class="regular-text" id="odwpedda_select_field_3" id="odwpedda_settings[odwpedda_select_field_3]" type="number">
+<?php
+    }
+endif;
+
+
+
 if ( !function_exists( 'odwpedda_admin_menu' ) ) :
     /**
      * Plugin's administration menu.
@@ -277,7 +369,7 @@ if ( ! function_exists( 'odwpedda_options_page' ) ) :
     function odwpedda_options_page() {
 ?>
 	<form action='options.php' method='post'>
-		<h2>Úpravy pro EDD</h2>
+		<h2>Úpravy pro Easy Digital Downloads plugin</h2>
 		<?php
 		settings_fields( ODWPEDDA_SLUG );
 		do_settings_sections( ODWPEDDA_SLUG );
